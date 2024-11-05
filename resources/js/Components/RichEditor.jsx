186d14bae46986f6731dadfcf5974be2dd9fc745
adolfjeremy <adolf.jer@gmail.com>
2024-11-05 10:41:54 +0700

    change rich text editor

--- resources/js/Components/RichEditor.jsx
+++ resources/js/Components/RichEditor.jsx
@@ -1,62 +1,354 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useTheme, Box } from "@mui/material";
-import "@/assets/sass/editor.scss";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+
+import {
+    ClassicEditor,
+    AccessibilityHelp,
+    Alignment,
+    Autoformat,
+    AutoImage,
+    AutoLink,
+    Autosave,
+    BalloonToolbar,
+    BlockQuote,
+    Bold,
+    CloudServices,
+    Code,
+    Essentials,
+    FindAndReplace,
+    FontBackgroundColor,
+    FontColor,
+    FontFamily,
+    FontSize,
+    GeneralHtmlSupport,
+    Heading,
+    Highlight,
+    HorizontalLine,
+    ImageBlock,
+    ImageCaption,
+    ImageInline,
+    ImageInsertViaUrl,
+    ImageResize,
+    ImageStyle,
+    ImageTextAlternative,
+    ImageToolbar,
+    ImageUpload,
+    Indent,
+    IndentBlock,
+    Italic,
+    Link,
+    LinkImage,
+    List,
+    ListProperties,
+    MediaEmbed,
+    Paragraph,
+    PasteFromOffice,
+    RemoveFormat,
+    SelectAll,
+    SpecialCharacters,
+    SpecialCharactersArrows,
+    SpecialCharactersCurrency,
+    SpecialCharactersEssentials,
+    SpecialCharactersLatin,
+    SpecialCharactersMathematical,
+    SpecialCharactersText,
+    Strikethrough,
+    Style,
+    Subscript,
+    Superscript,
+    Table,
+    TableCaption,
+    TableCellProperties,
+    TableColumnResize,
+    TableProperties,
+    TableToolbar,
+    TextTransformation,
+    TodoList,
+    Underline,
+    Undo,
+} from "ckeditor5";
+
 import "ckeditor5/ckeditor5.css";
 
 function RichEditor({ handleInput, value, placeholder }) {
     const theme = useTheme();
-    const [Editor, setEditor] = useState(null);
+    const editorContainerRef = useRef(null);
+    const editorRef = useRef(null);
+    const [isLayoutReady, setIsLayoutReady] = useState(false);
 
     useEffect(() => {
-        // Hanya load CKEditor di lingkungan klien
-        if (typeof window !== "undefined") {
-            Promise.all([
-                import("@ckeditor/ckeditor5-react").then(
-                    ({ CKEditor }) => CKEditor
-                ),
-                import("ckeditor5").then(
-                    ({
-                        ClassicEditor,
-                        Bold,
-                        Essentials,
-                        Italic,
-                        Paragraph,
-                        Undo,
-                    }) => ({
-                        ClassicEditor,
-                        Bold,
-                        Essentials,
-                        Italic,
-                        Paragraph,
-                        Undo,
-                    })
-                ),
-            ]).then(
-                ([
-                    CKEditor,
-                    {
-                        ClassicEditor,
-                        Bold,
-                        Essentials,
-                        Italic,
-                        Paragraph,
-                        Undo,
-                    },
-                ]) => {
-                    ClassicEditor.builtinPlugins = [
-                        Essentials,
-                        Bold,
-                        Italic,
-                        Paragraph,
-                        Undo,
-                    ];
-                    setEditor({ CKEditor, ClassicEditor });
-                }
-            );
-        }
+        setIsLayoutReady(true);
+
+        return () => setIsLayoutReady(false);
     }, []);
 
-    if (!Editor) return null; // Kembalikan null jika Editor belum siap
+    const editorConfig = {
+        toolbar: {
+            items: [
+                "undo",
+                "redo",
+                "|",
+                "heading",
+                "style",
+                "|",
+                "fontSize",
+                "fontFamily",
+                "fontColor",
+                "fontBackgroundColor",
+                "|",
+                "bold",
+                "italic",
+                "underline",
+                "|",
+                "link",
+                "insertTable",
+                "highlight",
+                "blockQuote",
+                "|",
+                "alignment",
+                "|",
+                "bulletedList",
+                "numberedList",
+                "todoList",
+                "outdent",
+                "indent",
+            ],
+            shouldNotGroupWhenFull: false,
+        },
+        plugins: [
+            AccessibilityHelp,
+            Alignment,
+            Autoformat,
+            AutoImage,
+            AutoLink,
+            Autosave,
+            BalloonToolbar,
+            BlockQuote,
+            Bold,
+            CloudServices,
+            Code,
+            Essentials,
+            FindAndReplace,
+            FontBackgroundColor,
+            FontColor,
+            FontFamily,
+            FontSize,
+            GeneralHtmlSupport,
+            Heading,
+            Highlight,
+            HorizontalLine,
+            ImageBlock,
+            ImageCaption,
+            ImageInline,
+            ImageInsertViaUrl,
+            ImageResize,
+            ImageStyle,
+            ImageTextAlternative,
+            ImageToolbar,
+            ImageUpload,
+            Indent,
+            IndentBlock,
+            Italic,
+            Link,
+            LinkImage,
+            List,
+            ListProperties,
+            MediaEmbed,
+            Paragraph,
+            PasteFromOffice,
+            RemoveFormat,
+            SelectAll,
+            SpecialCharacters,
+            SpecialCharactersArrows,
+            SpecialCharactersCurrency,
+            SpecialCharactersEssentials,
+            SpecialCharactersLatin,
+            SpecialCharactersMathematical,
+            SpecialCharactersText,
+            Strikethrough,
+            Style,
+            Subscript,
+            Superscript,
+            Table,
+            TableCaption,
+            TableCellProperties,
+            TableColumnResize,
+            TableProperties,
+            TableToolbar,
+            TextTransformation,
+            TodoList,
+            Underline,
+            Undo,
+        ],
+        balloonToolbar: [
+            "bold",
+            "italic",
+            "|",
+            "link",
+            "|",
+            "bulletedList",
+            "numberedList",
+        ],
+        fontFamily: {
+            supportAllValues: true,
+        },
+        fontSize: {
+            options: [10, 12, 14, "default", 18, 20, 22],
+            supportAllValues: true,
+        },
+        heading: {
+            options: [
+                {
+                    model: "paragraph",
+                    title: "Paragraph",
+                    class: "ck-heading_paragraph",
+                },
+                {
+                    model: "heading1",
+                    view: "h1",
+                    title: "Heading 1",
+                    class: "ck-heading_heading1",
+                },
+                {
+                    model: "heading2",
+                    view: "h2",
+                    title: "Heading 2",
+                    class: "ck-heading_heading2",
+                },
+                {
+                    model: "heading3",
+                    view: "h3",
+                    title: "Heading 3",
+                    class: "ck-heading_heading3",
+                },
+                {
+                    model: "heading4",
+                    view: "h4",
+                    title: "Heading 4",
+                    class: "ck-heading_heading4",
+                },
+                {
+                    model: "heading5",
+                    view: "h5",
+                    title: "Heading 5",
+                    class: "ck-heading_heading5",
+                },
+                {
+                    model: "heading6",
+                    view: "h6",
+                    title: "Heading 6",
+                    class: "ck-heading_heading6",
+                },
+            ],
+        },
+        htmlSupport: {
+            allow: [
+                {
+                    name: /^.*$/,
+                    styles: true,
+                    attributes: true,
+                    classes: true,
+                },
+            ],
+        },
+        image: {
+            toolbar: [
+                "toggleImageCaption",
+                "imageTextAlternative",
+                "|",
+                "imageStyle:inline",
+                "imageStyle:wrapText",
+                "imageStyle:breakText",
+                "|",
+                "resizeImage",
+            ],
+        },
+        initialData: value,
+        link: {
+            addTargetToExternalLinks: true,
+            defaultProtocol: "https://",
+            decorators: {
+                toggleDownloadable: {
+                    mode: "manual",
+                    label: "Downloadable",
+                    attributes: {
+                        download: "file",
+                    },
+                },
+            },
+        },
+        list: {
+            properties: {
+                styles: true,
+                startIndex: true,
+                reversed: true,
+            },
+        },
+        menuBar: {
+            isVisible: true,
+        },
+        placeholder: placeholder,
+        style: {
+            definitions: [
+                {
+                    name: "Article category",
+                    element: "h3",
+                    classes: ["category"],
+                },
+                {
+                    name: "Title",
+                    element: "h2",
+                    classes: ["document-title"],
+                },
+                {
+                    name: "Subtitle",
+                    element: "h3",
+                    classes: ["document-subtitle"],
+                },
+                {
+                    name: "Info box",
+                    element: "p",
+                    classes: ["info-box"],
+                },
+                {
+                    name: "Side quote",
+                    element: "blockquote",
+                    classes: ["side-quote"],
+                },
+                {
+                    name: "Marker",
+                    element: "span",
+                    classes: ["marker"],
+                },
+                {
+                    name: "Spoiler",
+                    element: "span",
+                    classes: ["spoiler"],
+                },
+                {
+                    name: "Code (dark)",
+                    element: "pre",
+                    classes: ["fancy-code", "fancy-code-dark"],
+                },
+                {
+                    name: "Code (bright)",
+                    element: "pre",
+                    classes: ["fancy-code", "fancy-code-bright"],
+                },
+            ],
+        },
+        table: {
+            contentToolbar: [
+                "tableColumn",
+                "tableRow",
+                "mergeTableCells",
+                "tableProperties",
+                "tableCellProperties",
+            ],
+        },
+    };
+
     return (
         <Box
             sx={{
@@ -65,26 +357,33 @@ function RichEditor({ handleInput, value, placeholder }) {
                 alignItems: "start",
                 justifyContent: "center",
                 gap: 1,
-                mb: 3,
-                ".ck.ck-editor": {
-                    width: "100%",
+                my: 2,
+                ".editor-container_classic-editor .editor-container__editor": {
+                    maxWidth: "calc(100vw - 358px)",
                 },
             }}
         >
-            <label>{placeholder}</label>
-            <Editor.CKEditor
-                editor={Editor.ClassicEditor}
-                onChange={(event, editor) => {
-                    const data = editor.getData();
-                    handleInput(data);
-                }}
-                config={{
-                    toolbar: {
-                        items: ["undo", "redo", "|", "bold", "italic"],
-                    },
-                    initialData: value,
-                }}
-            />
+            <div className="main-container">
+                <div
+                    className="editor-container editor-container_classic-editor editor-container_include-style"
+                    ref={editorContainerRef}
+                >
+                    <div className="editor-container__editor">
+                        <div ref={editorRef}>
+                            {isLayoutReady && (
+                                <CKEditor
+                                    editor={ClassicEditor}
+                                    config={editorConfig}
+                                    onChange={(event, editor) => {
+                                        const data = editor.getData();
+                                        handleInput(data);
+                                    }}
+                                />
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Box>
     );
 }
