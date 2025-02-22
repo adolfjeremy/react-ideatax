ec1ec2439d0147ccf84d9bd49fd3eef35c001af1
adolfjeremy <adolf.jer@email.com>
2025-02-22 14:46:48 +0700

    fixing language selection alogrithm

--- resources/js/utils/checkLang.js
+++ resources/js/utils/checkLang.js
@@ -1,13 +1,8 @@
-const checkLang = (lang, cEn, cId, cJp, cCn) =>
-    lang == "en"
-        ? cEn
-        : lang == "id"
-        ? cId
-        : lang == "jp"
-        ? cJp
-        : cJp == ""
-        ? cEn
-        : lang == "zh-CN"
-        ? cCn
-        : cEn;
+const checkLang = (lang, cEn, cId, cJp, cCn) => {
+    if (lang === "en") return cEn;
+    if (lang === "id") return cId;
+    if (lang === "jp") return cJp || cEn; // Jika cJp kosong atau null, gunakan cEn
+    if (lang === "zh-CN") return cCn || cEn; // Jika cCn kosong atau null, gunakan cEn
+    return cEn; // Default ke bahasa Inggris jika tidak ada yang cocok
+};
 export default checkLang;
