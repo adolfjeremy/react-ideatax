ce6f580f7913ede87f60b494ff65299c0cbf5e8e
adolfjeremy <adolf.jer@email.com>
2025-05-14 19:45:22 +0700

    teadf

--- resources/js/Pages/Home/Home.jsx
+++ resources/js/Pages/Home/Home.jsx
@@ -130,53 +130,6 @@ function Home() {
                             </Typography>
                         </div>
                     </div>
-                    {/* <div ref={containerRef} className="row">
-                        {stats.map((stat) => (
-                            <Box
-                                key={stat.id}
-                                className="col-4 col-md-3"
-                                sx={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                    justifyContent: "start",
-                                    flexDirection: "column",
-                                }}
-                            >
-                                <Counter target={stat.value} />
-
-                                <Typography
-                                    sx={{
-                                        color: theme.palette.custom.orange,
-                                        fontSize: "1.25rem",
-                                        lineHeight: "1.390625em",
-                                        textAlign: "center",
-                                        fontWeight: "500",
-                                        textTransform: "capitalize",
-                                        [theme.breakpoints.down("md")]: {
-                                            fontSize: "1rem",
-                                        },
-                                    }}
-                                    className="stat_head m-0"
-                                >
-                                    {isMobile && stat.id === 3
-                                        ? checkLang(
-                                              locale,
-                                              "Experience",
-                                              "Pengalaman",
-                                              "",
-                                              stat.head_ch
-                                          )
-                                        : checkLang(
-                                              locale,
-                                              stat.head_eng,
-                                              stat.head,
-                                              stat.head_jpn,
-                                              stat.head_ch
-                                          )}
-                                </Typography>
-                            </Box>
-                        ))}
-                    </div> */}
                     <div className="row">
                         <div className="col-12 text-center mt-5">
                             <Typography
