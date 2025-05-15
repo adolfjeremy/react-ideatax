0a78cac5299747f0a4833cd76c102d24a6331e88
adolfjeremy <adolf.jer@email.com>
2025-05-15 10:28:13 +0700

    code splitting articleDetail

--- resources/js/Pages/Article/Detail/ArticleDetail.jsx
+++ resources/js/Pages/Article/Detail/ArticleDetail.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import { Box, Typography, useTheme, Link } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import { usePage } from "@inertiajs/react";
 import Guest from "@/Layout/Guest";
-import checkLang from "@/utils/checkLang";
+import DetailHero from "./parts/DetailHero";
 import RichText from "@/Components/RichText";
-import formatDate from "@/utils/formatDate";
 import CommentSection from "@/Components/CommentSection";
 import Pagination from "@/Components/Pagination";
+import checkLang from "@/utils/checkLang";
+import formatDate from "@/utils/formatDate";
 
 import "../article.scss";
 
@@ -55,101 +55,13 @@ function ArticleDetail() {
                     : route("article-detail.jp", item.slug_eng)
             )}
         >
-            <Box
-                component="section"
-                sx={{
-                    position: "relative",
-                    [theme.breakpoints.up("md")]: {
-                        maxHeight: "100vh",
-                        overflow: "hidden",
-                    },
-                    [theme.breakpoints.down("md")]: {
-                        paddingTop: "100px",
-                    },
-                }}
-            >
-                <div>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img
-                                src={`/storage/${item.photo}`}
-                                className="d-block w-100"
-                                alt="title"
-                            />
-                            <Box
-                                sx={{
-                                    position: "absolute",
-                                    left: 0,
-                                    bottom: 0,
-                                    top: 0,
-                                    right: "35vw",
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    alignItems: "flex-start",
-                                    justifyContent: "center",
-                                    paddingLeft: "3rem",
-                                    gap: "0.5rem",
-                                }}
-                            >
-                                <Typography
-                                    sx={{
-                                        color: theme.palette.custom.darkBlue,
-                                        fontSize: "4.0325rem",
-                                        [theme.breakpoints.down("md")]: {
-                                            fontSize: "3rem",
-                                        },
-                                        [theme.breakpoints.down("sm")]: {
-                                            fontSize: " 1.125rem",
-                                        },
-                                        lineHeight: "1.01561707em",
-                                        letterSpacing: "0.009em",
-                                        fontWeight: "700",
-                                    }}
-                                    variant="h1"
-                                >
-                                    {checkLang(
-                                        locale,
-                                        item.title_eng,
-                                        item.title,
-                                        item.title_jpn
-                                    )}
-                                </Typography>
-                                <Link
-                                    sx={{
-                                        fontSize: "1.9751rem",
-                                        color: theme.palette.custom.orange,
-                                        textDecoration: "none",
-                                        lineHeight: "1.1875em",
-                                        letterSpacing: "-0.054em",
-                                        fontWeight: "700",
-                                        [theme.breakpoints.down("sm")]: {
-                                            fontSize: "0.8",
-                                        },
-                                    }}
-                                    href="#"
-                                >
-                                    {item.article_category.title}
-                                </Link>
-                                <Typography
-                                    sx={{
-                                        color: theme.palette.custom.lightBlue,
-                                        fontSize: "1.4459rem",
-                                        [theme.breakpoints.down("sm")]: {
-                                            fontSize: "0.8rem",
-                                        },
-                                        margin: 0,
-                                        padding: 0,
-                                        lineHeight: "1.42498753em",
-                                        letterSpacing: "0.009em",
-                                    }}
-                                >
-                                    {formatDate(item.created_at)}
-                                </Typography>
-                            </Box>
-                        </div>
-                    </div>
-                </div>
-            </Box>
+            <DetailHero
+                item={item}
+                theme={theme}
+                formatDate={formatDate}
+                locale={locale}
+                checkLang={checkLang}
+            />
             <Box component="article">
                 <div className="container">
                     <div className="row px-md-5">
---
+++ resources/js/Pages/Article/Detail/parts/DetailHero.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { Box, Typography, Link } from "@mui/material";
+
+function DetailHero({ item, formatDate, checkLang, locale, theme }) {
+    return (
+        <Box
+            component="section"
+            sx={{
+                position: "relative",
+                [theme.breakpoints.up("md")]: {
+                    maxHeight: "100vh",
+                    overflow: "hidden",
+                },
+                [theme.breakpoints.down("md")]: {
+                    paddingTop: "100px",
+                },
+            }}
+        >
+            <div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img
+                            src={`/storage/${item.photo}`}
+                            className="d-block w-100"
+                            alt="title"
+                        />
+                        <Box
+                            sx={{
+                                position: "absolute",
+                                left: 0,
+                                bottom: 0,
+                                top: 0,
+                                right: "35vw",
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "flex-start",
+                                justifyContent: "center",
+                                paddingLeft: "3rem",
+                                gap: "0.5rem",
+                            }}
+                        >
+                            <Typography
+                                sx={{
+                                    color: theme.palette.custom.darkBlue,
+                                    fontSize: "4.0325rem",
+                                    [theme.breakpoints.down("md")]: {
+                                        fontSize: "3rem",
+                                    },
+                                    [theme.breakpoints.down("sm")]: {
+                                        fontSize: " 1.125rem",
+                                    },
+                                    lineHeight: "1.01561707em",
+                                    letterSpacing: "0.009em",
+                                    fontWeight: "700",
+                                }}
+                                variant="h1"
+                            >
+                                {checkLang(
+                                    locale,
+                                    item.title_eng,
+                                    item.title,
+                                    item.title_jpn
+                                )}
+                            </Typography>
+                            <Link
+                                sx={{
+                                    fontSize: "1.9751rem",
+                                    color: theme.palette.custom.orange,
+                                    textDecoration: "none",
+                                    lineHeight: "1.1875em",
+                                    letterSpacing: "-0.054em",
+                                    fontWeight: "700",
+                                    [theme.breakpoints.down("sm")]: {
+                                        fontSize: "0.8",
+                                    },
+                                }}
+                                href="#"
+                            >
+                                {item.article_category.title}
+                            </Link>
+                            <Typography
+                                sx={{
+                                    color: theme.palette.custom.lightBlue,
+                                    fontSize: "1.4459rem",
+                                    [theme.breakpoints.down("sm")]: {
+                                        fontSize: "0.8rem",
+                                    },
+                                    margin: 0,
+                                    padding: 0,
+                                    lineHeight: "1.42498753em",
+                                    letterSpacing: "0.009em",
+                                }}
+                            >
+                                {formatDate(item.created_at)}
+                            </Typography>
+                        </Box>
+                    </div>
+                </div>
+            </div>
+        </Box>
+    );
+}
+
+export default DetailHero;
